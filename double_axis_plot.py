ed3ce1b5f9569f358281fb1ca28f1649c0de5238
olfj <ole.fjeldsaa@live.com>
2023-12-01 15:49:54 +0100

    Small bug fixes

--- double_axis_plot.py
+++ double_axis_plot.py
@@ -15,7 +15,7 @@ rng = np.random.default_rng(seed=42)
 average = 50
 pwr = PWR(n, dim, n_initial_neutrons, speed, rng, plot_data=False)
 
-length = 4000
+length = 10049
 
 gens = np.arange(length)
 temps = np.zeros(length)
@@ -35,7 +35,7 @@ gens = np.arange(length-49)
 x = np.linspace(0, 2 * np.pi, 400)
 y = np.sin(x ** 2)
 
-fig = plt.figure(figsize=())
+fig = plt.figure()
 gs = GridSpec(nrows=2, ncols=1)
 axleft = fig.add_subplot(gs[0, 0])
 axleft0 = axleft
@@ -45,13 +45,13 @@ axleft1 = axleft0.twinx()
 color = 'tab:orange'
 axleft0.set_xlabel('Time steps')
 axleft0.set_ylabel('Temperature ⁰K', color=color)
-axleft0.plot(gens[:3000], temps[:3000], color=color)
+axleft0.plot(gens[:2000], temps[:2000], color=color)
 axleft0.tick_params(axis='y', labelcolor=color)
 
 # Plot the second function on the right y-axis
 color = 'tab:blue'
 axleft1.set_ylabel('Reactivity', color=color)
-axleft1.plot(gens[:3000], reactivities[:3000], color=color)
+axleft1.plot(gens[:2000], reactivities[:2000], color=color)
 axleft1.tick_params(axis='y', labelcolor=color)
 
 
@@ -63,13 +63,13 @@ axright1 = axright0.twinx()
 color = 'tab:orange'
 axright0.set_xlabel('Time steps')
 axright0.set_ylabel('Temperature ⁰K', color=color)
-axright0.plot(gens[3000:], temps[3000:], color=color)
+axright0.plot(gens[2000:], temps[2000:], color=color)
 axright0.tick_params(axis='y', labelcolor=color)
 
 # Plot the second function on the right y-axis
 color = 'tab:blue'
 axright1.set_ylabel('Reactivity', color=color)
-axright1.plot(gens[3000:], reactivities[3000:], color=color)
+axright1.plot(gens[2000:], reactivities[2000:], color=color)
 axright1.tick_params(axis='y', labelcolor=color)
 
 
