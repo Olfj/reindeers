e50f5548bfcd8e9279251b752757b361b18ab862
olfj <ole.fjeldsaa@live.com>
2023-12-07 14:45:38 +0100

    Updated how plotting is done. Only one plot in showint the entire simulation.

--- plot_T_v_r.py
+++ plot_T_v_r.py
@@ -14,9 +14,9 @@ speed = 1
 rng = np.random.default_rng(seed=42)
 average = 50
 
-pwr = PWR(n, dim, n_initial_neutrons, speed, seed=rng, plot_data=False)
+pwr = PWR(n, dim, n_initial_neutrons, speed, seed=rng, plot_data=False, absorb_rate=0.021)
 
-length = 3049
+length = 10049
 
 gens = np.arange(length)
 temps = np.zeros(length)
@@ -49,26 +49,36 @@ axleft1 = axleft0.twinx()
 
 axleft0.set_xlabel('Time steps')
 axleft0.set_ylabel('Temperature ⁰K', color=color_left)
-axleft0.plot(gens[:2000], temps[:2000], color=color_left)
+axleft0.plot(gens, temps, color=color_left)
 axleft0.tick_params(axis='y', labelcolor=color_left)
 
 axleft1.set_ylabel('Reactivity', color=color_right)
-axleft1.plot(gens[:2000], reactivities[:2000], color=color_right)
+axleft1.plot(gens, reactivities, color=color_right)
 axleft1.tick_params(axis='y', labelcolor=color_right)
 
 
-axright = fig.add_subplot(gs[1, 0])
-axright0 = axright
-axright1 = axright0.twinx()
+# axleft0.set_xlabel('Time steps')
+# axleft0.set_ylabel('Temperature ⁰K', color=color_left)
+# axleft0.plot(gens[:1000], temps[:1000], color=color_left)
+# axleft0.tick_params(axis='y', labelcolor=color_left)
 
-axright0.set_xlabel('Time steps')
-axright0.set_ylabel('Temperature ⁰K', color=color_left)
-axright0.plot(gens[2000:], temps[2000:], color=color_left)
-axright0.tick_params(axis='y', labelcolor=color_left)
+# axleft1.set_ylabel('Reactivity', color=color_right)
+# axleft1.plot(gens[:1000], reactivities[:1000], color=color_right)
+# axleft1.tick_params(axis='y', labelcolor=color_right)
 
-axright1.set_ylabel('Reactivity', color=color_right)
-axright1.plot(gens[2000:], reactivities[2000:], color=color_right)
-axright1.tick_params(axis='y', labelcolor=color_right)
+
+# axright = fig.add_subplot(gs[1, 0])
+# axright0 = axright
+# axright1 = axright0.twinx()
+
+# axright0.set_xlabel('Time steps')
+# axright0.set_ylabel('Temperature ⁰K', color=color_left)
+# axright0.plot(gens[4000:4500], temps[4000:4500], color=color_left)
+# axright0.tick_params(axis='y', labelcolor=color_left)
+
+# axright1.set_ylabel('Reactivity', color=color_right)
+# axright1.plot(gens[4000:4500], reactivities[4000:4500], color=color_right)
+# axright1.tick_params(axis='y', labelcolor=color_right)
 
 
 plt.show()
