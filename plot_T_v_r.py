89bdd8db8ef92acecba0fde2ddc5324a3fba4c48
olfj <ole.fjeldsaa@live.com>
2024-01-04 13:46:38 +0100

    Updated axis colors and labels to make plots more readable.

--- plot_T_v_r.py
+++ plot_T_v_r.py
@@ -14,71 +14,51 @@ speed = 1
 rng = np.random.default_rng(seed=42)
 average = 50
 
-pwr = PWR(n, dim, n_initial_neutrons, speed, seed=rng, plot_data=False, absorb_rate=0.021)
+absorption_rates = [0.0215]
 
-length = 10049
+for rate in absorption_rates:
 
-gens = np.arange(length)
-temps = np.zeros(length)
-reactivities = np.zeros(length)
+    pwr = PWR(n, dim, n_initial_neutrons, speed, seed=rng, plot_data=False, absorb_rate=rate)
 
-for gen in tqdm(gens):
-    reactivities[gen] = pwr.get_reactivity()
-    temps[gen] = pwr.temperature
-    pwr.update(1)
+    length = 100049
 
+    gens = np.arange(length)
+    temps = np.zeros(length)
+    reactivities = np.zeros(length)
 
-reactivities = moving_average(reactivities)
-temps = moving_average(temps)
-gens = np.arange(length-49)
+    for gen in tqdm(gens):
+        reactivities[gen] = pwr.get_reactivity()
+        temps[gen] = pwr.temperature
+        pwr.update(1)
+        if pwr.temperature > 670:
+            break
 
-# Some example data to display
-x = np.linspace(0, 2 * np.pi, 400)
-y = np.sin(x ** 2)
+    reactivities = moving_average(reactivities)
+    temps = moving_average(temps)
+    gens = np.arange(length - 49)
 
-fig = plt.figure()
-gs = GridSpec(nrows=2, ncols=1)
+    for iterations in [(0, 500), (0, 1000), (0, 5000), (0, 10000), (2000, 2500), (0, 20000), (0, 50000), (0, 100000)]:
 
-color_left = 'tab:orange'
-color_right = 'tab:blue'
+        fig, ax_left = plt.subplots(figsize=(12, 5))
 
-axleft = fig.add_subplot(gs[0, 0])
-axleft0 = axleft
-axleft1 = axleft0.twinx() 
+        color_left = 'tab:orange'
+        color_right = 'tab:blue'
 
+        ax_right = ax_left.twinx()
 
-axleft0.set_xlabel('Time steps')
-axleft0.set_ylabel('Temperature ⁰K', color=color_left)
-axleft0.plot(gens, temps, color=color_left)
-axleft0.tick_params(axis='y', labelcolor=color_left)
+        ax_left.set_xlabel('Time steps')
+        ax_left.set_title('Temperature (K) and reactivity. Absorption rate= ' + str(rate))
+        ax_left.set_ylabel('K')
+        line1, = ax_left.plot(gens[iterations[0]:iterations[1]], temps[iterations[0]:iterations[1]], color=color_left, label='K')
+        ax_left.tick_params(axis='y')
 
-axleft1.set_ylabel('Reactivity', color=color_right)
-axleft1.plot(gens, reactivities, color=color_right)
-axleft1.tick_params(axis='y', labelcolor=color_right)
+        ax_right.set_ylabel('Reactivity')
+        line2, = ax_right.plot(gens[iterations[0]:iterations[1]], reactivities[iterations[0]:iterations[1]], label='Reactivity')
+        ax_right.tick_params(axis='y')
 
+        lines = [line1, line2]
+        ax_left.legend(lines, [line.get_label() for line in lines], loc='upper left', bbox_to_anchor=(0.4, -0.2), ncol=2)
 
-# axleft0.set_xlabel('Time steps')
-# axleft0.set_ylabel('Temperature ⁰K', color=color_left)
-# axleft0.plot(gens[:1000], temps[:1000], color=color_left)
-# axleft0.tick_params(axis='y', labelcolor=color_left)
-
-# axleft1.set_ylabel('Reactivity', color=color_right)
-# axleft1.plot(gens[:1000], reactivities[:1000], color=color_right)
-# axleft1.tick_params(axis='y', labelcolor=color_right)
-
-
-# axright = fig.add_subplot(gs[1, 0])
-# axright0 = axright
-# axright1 = axright0.twinx()
-
-# axright0.set_xlabel('Time steps')
-# axright0.set_ylabel('Temperature ⁰K', color=color_left)
-# axright0.plot(gens[4000:4500], temps[4000:4500], color=color_left)
-# axright0.tick_params(axis='y', labelcolor=color_left)
-
-# axright1.set_ylabel('Reactivity', color=color_right)
-# axright1.plot(gens[4000:4500], reactivities[4000:4500], color=color_right)
-# axright1.tick_params(axis='y', labelcolor=color_right)
-
-
-plt.show()
+        plt.tight_layout()
+        plt.savefig('plots/T_v_r/Absorption rate = ' + str(rate) + 'iterations = ' + str(iterations[1]) + '.png', bbox_inches='tight')
+        plt.close()
